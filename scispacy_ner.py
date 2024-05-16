e5044d356618e8aead37dd88cdf1468c2185b2bc
Rafael Goncalves <rsgoncalves@gmx.com>
2024-05-15 20:31:51 -0400

    Add functionality to load and do NER in tables

--- scispacy_ner.py
+++ scispacy_ner.py
@@ -9,13 +9,12 @@ import pandas as pd
 from tqdm import tqdm
 from text2term import onto_utils
 from scispacy.linking import EntityLinker
-from scispacy.abbreviation import AbbreviationDetector
 from named_entity import LinkedNamedEntity
 import nltk
 
 nltk.download('punkt')
 
-__version__ = "0.6.3"
+__version__ = "0.7.0"
 
 
 class ScispacyUmlsNer:
@@ -28,8 +27,7 @@ class ScispacyUmlsNer:
         self._log.info(f"Loading scispaCy model {model}...")
         self._ner = spacy.load(self._model)
 
-        # Add abbreviation detector and UMLS linker
-        self._ner.add_pipe("abbreviation_detector")
+        # Add UMLS linking pipe
         self._ner.add_pipe("scispacy_linker", config={"resolve_abbreviations": True,
                                                       "linker_name": "umls",
                                                       "filter_for_definitions": False,
@@ -42,13 +40,14 @@ class ScispacyUmlsNer:
             "https://lhncbc.nlm.nih.gov/ii/tools/MetaMap/Docs/SemanticTypes_2018AB.txt",
             sep="|", names=['abbv', 'tui', 'label'])
 
-    def extract_entities(self, text, output_as_df=False, incl_unlinked_entities=False):
-        temp_entity_id = shortuuid.ShortUUID().random(length=10)
-        truecased_text = truecase.get_true_case(text)
+    def extract_entities(self, input_text, input_id="", output_as_df=False, incl_unlinked_entities=False):
+        if input_id == "":
+            input_id = shortuuid.ShortUUID().random(length=10)
+        truecased_text = truecase.get_true_case(input_text)
         entities = []
         doc = self._ner(text=truecased_text)
         if len(doc.ents) == 0:
-            self._log.debug(f"No named entities found in text: {text}")
+            self._log.debug(f"No named entities found in text: {input_text}")
         for entity in doc.ents:  # Extract named entities and link them to UMLS
             linker = self._ner.get_pipe("scispacy_linker")
             if len(entity._.kb_ents) > 0:
@@ -56,43 +55,50 @@ class ScispacyUmlsNer:
                     cui, score = umls_entity
                     score = round(score, 3)
                     details = linker.kb.cui_to_entity[umls_entity[0]]
-                    self._add_entity_to_output(output=entities, input_id=temp_entity_id, input_text=text,
+                    self._add_entity_to_output(output=entities, input_id=input_id, input_text=input_text,
                                                entity=entity.text, entity_type=entity.label_, umls_cui=cui,
                                                umls_label=details.canonical_name, umls_semantic_types=details.types,
                                                umls_definition=details.definition,
                                                umls_synonyms=", ".join(details.aliases), umls_mapping_score=score)
             else:
-                self._log.debug(f"No UMLS mappings found for entity: {text}")
+                self._log.debug(f"No UMLS mappings found for entity: {input_text}")
                 if incl_unlinked_entities:
-                    self._add_entity_to_output(output=entities, input_id=temp_entity_id, input_text=text,
+                    self._add_entity_to_output(output=entities, input_id=input_id, input_text=input_text,
                                                entity=entity.text, entity_type=entity.label_)
-        if output_as_df:
-            return pd.DataFrame(entities)
-        else:
-            return entities
+        return pd.DataFrame(entities) if output_as_df else entities
 
     def extract_entities_in_list(self, string_list, output_as_df=False, incl_unlinked_entities=False):
+        self._log.info(f"Processing list of {len(string_list)} strings...")
         entities = []
         for string in tqdm(string_list):
-            entities.extend(self.extract_entities(string, incl_unlinked_entities=incl_unlinked_entities))
-        if output_as_df:
-            return pd.DataFrame(entities)
-        else:
-            return entities
+            entities.extend(self.extract_entities(string, incl_unlinked_entities=incl_unlinked_entities,
+                                                  output_as_df=False))
+        return pd.DataFrame(entities) if output_as_df else entities
 
     def extract_entities_in_file(self, filepath, output_as_df=False, incl_unlinked_entities=False):
-        self._log.info(f"Processing {filepath}...")
+        self._log.info(f"Processing file {filepath}...")
         entities = []
         with open(filepath, 'r') as file:
             lines = file.readlines()
             for line in tqdm(lines):
                 line = line.replace("\n", "")
                 if line != "":
-                    entities.extend(self.extract_entities(line, incl_unlinked_entities=incl_unlinked_entities))
-        if output_as_df:
-            return pd.DataFrame(entities)
-        else:
-            return entities
+                    entities.extend(self.extract_entities(line, incl_unlinked_entities=incl_unlinked_entities,
+                                                          output_as_df=False))
+        return pd.DataFrame(entities) if output_as_df else entities
+
+    def extract_entities_in_table(self, filepath, input_text_col, input_id_col="", input_col_sep="\t",
+                                  output_as_df=False, incl_unlinked_entities=False):
+        self._log.info(f"Processing table {filepath}...")
+        entities = []
+        input_table = pd.read_csv(filepath, sep=input_col_sep)
+        for index, row in tqdm(input_table.iterrows(), total=input_table.shape[0]):
+            input_text = row[input_text_col]
+            input_id = row[input_id_col]
+            if not pd.isna(input_text):
+                entities.extend(self.extract_entities(input_text=input_text, input_id=input_id, output_as_df=False,
+                                                      incl_unlinked_entities=incl_unlinked_entities))
+        return pd.DataFrame(entities) if output_as_df else entities
 
     def _add_entity_to_output(self, output, input_id, input_text, entity, entity_type, umls_cui="", umls_label="",
                               umls_definition="", umls_semantic_types=(), umls_synonyms="", umls_mapping_score=""):
@@ -114,6 +120,8 @@ class ScispacyUmlsNer:
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("scispacy_ner")
     parser.add_argument("-i", "--input", required=True, type=str, help="Input file")
+    parser.add_argument("-c", "--col", type=str, help="Table column with input text")
+    parser.add_argument("-d", "--id", type=str, help="Table column with input text IDs")
     parser.add_argument("-m", "--model", default="en_core_sci_scibert", type=str,
                         help="Name of the scispaCy model to be used")
     args = parser.parse_args()
@@ -129,5 +137,12 @@ if __name__ == '__main__':
     my_scispacy = ScispacyUmlsNer(model=input_model)
 
     # extract entities in the given input file
-    entities_df = my_scispacy.extract_entities_in_file(filepath=input_file, output_as_df=True)
+    if ".tsv" in input_file:
+        entities_df = my_scispacy.extract_entities_in_table(filepath=input_file, input_col_sep="\t", output_as_df=True,
+                                                            input_text_col=args.col, input_id_col=args.id)
+    elif ".csv" in input_file:
+        entities_df = my_scispacy.extract_entities_in_table(filepath=input_file, input_col_sep=",", output_as_df=True,
+                                                            input_text_col=args.col, input_id_col=args.id)
+    else:
+        entities_df = my_scispacy.extract_entities_in_file(filepath=input_file, output_as_df=True)
     entities_df.to_csv(output_file_path, sep="\t", index=False)
